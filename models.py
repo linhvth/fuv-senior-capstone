c039e0cd152113a6df440ce22cd0106cb7e0766b
linhvth <vuthihonglinh.work@gmail.com>
2024-03-19 00:26:04 +0700

    placeholder

---
+++ models.py
@@ -0,0 +1,11 @@
+"""
+Description: 
+    This Python file implements a collection of simple machine learning 
+    models for experimentation purposes. These models can be used as building 
+    blocks for more complex architectures or for exploring various algorithms 
+    on a particular dataset.
+
+Models: SVM, Perceptron
+
+Author: Linh Vu (2024)
+"""
