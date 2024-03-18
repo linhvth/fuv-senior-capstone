2faf29c0f1bb2a6efea9c1b56c3f189454d250de
linhvth <vuthihonglinh.work@gmail.com>
2024-03-18 23:06:08 +0700

    create some functions to test main program

--- main.py
+++ main.py
@@ -6,31 +6,24 @@ Linh Vu (2024)
 
 ### IMPORT LIBRARIES
 
-import time             # control the execution time
-import numpy as np      # LinAlg/ matrices manipulation
-import autograd.numpy as au
+import time             
+import numpy as np      
 import math
-from autograd import grad, jacobian   # get hessian
 from numpy.linalg import norm
 
-from autograd import elementwise_grad as egrad  # for functions that vectorize over inputs
-import matplotlib.pyplot as plt
+### Test 1: Simple convex functions
+f = lambda x: x**2
+df = lambda x: 2*x
 
-def tanh(x):                 # Define a function
-    y = np.exp(-2.0 * x)
-    return (1.0 - y) / (1.0 + y)
 
-x = np.linspace(-7, 7, 200)
+### Test 2: Non-convex functions
+def rosenbrock(X):
+  """
+  Rosenbrock function: 
+    f(x) = sum((a_i - x[i-1])^2) for i = 1 to n-1, a = [1, 1.2, ..., n]
+  """
+  a = np.array([1] + [1.2 for _ in range(len(X) - 1)])
+  return np.sum((a - X[:-1])**2) + 100 * np.sum((X[1:] - a**2)**2)
 
-plt.plot(x, tanh(x),
-          x, egrad(tanh)(x),                                     # first  derivative
-          x, egrad(egrad(tanh))(x),                              # second derivative
-          x, egrad(egrad(egrad(tanh)))(x),                       # third  derivative
-          x, egrad(egrad(egrad(egrad(tanh))))(x),                # fourth derivative
-          x, egrad(egrad(egrad(egrad(egrad(tanh)))))(x),         # fifth  derivative
-          x, egrad(egrad(egrad(egrad(egrad(egrad(tanh))))))(x))  # sixth  derivative
 
-plt.show()
-print("hi")
-def main():
-    return
+### Test 3: Perceptron
