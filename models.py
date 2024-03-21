5cf3c54a6df3a7f5ea999240805ea3be2f716f10
linhvth <vuthihonglinh.work@gmail.com>
2024-03-22 02:16:21 +0700

    complete LogReg Implementation

--- models.py
+++ models.py
@@ -13,18 +13,27 @@ Author: Linh Vu (2024)
 """
 
 import numpy as np
+from sgd import *
 
 class LogisticRegression:
     """
     This implementation assume that the value of y is either 0 or 1
     """
-    def __init__(self, optimizer) -> None:
-        self.theta = None           # placeholder for params
-        self.optimizer = optimizer  # optimizer
+    def __init__(self):
+        self.theta = None      # placeholder for params
+        self.optimizer = None  # optimizer
     
-    def fit(self, M, y, optimizer):
+    def fit(self, M, y, optimizer, n_iters=1000, n_streams=100, step_size=0.01, 
+            update_method='avgLastPoint'):
+        init_theta = np.random.randn(M.shape[1])
 
-        return 
+        self.optimizer = optimizer
+        self.optimizer.fit(f=self._loss_function_single, df=self._loss_gradient_single, 
+                           init_theta=init_theta, M=M, y=y, n_iters=n_iters, n_streams=n_streams, 
+                           step_size=step_size, update_method=update_method)
+        
+        # Get optimal theta from optimizer
+        self.theta = optimizer.get_theta()
     
     def predict(self, M, threshold=0.5):
         """
@@ -42,10 +51,10 @@ class LogisticRegression:
     def _sigmoid_function(self, value):
         return 1/(1 + np.exp(-value))
     
-    def _loss_function_single(self, m, y):
-        z = self.theta.T@m
+    def _loss_function_single(self, m, y, theta):
+        z = theta.T@m
         return - y*z + np.log(1 + np.exp(z))
     
-    def _loss_gradient_single(self, m, y):
-        z = self.theta.T@m
+    def _loss_gradient_single(self, m, y, theta):
+        z = theta.T@m
         return (-y + 1/(1+np.exp(-z)))*m
