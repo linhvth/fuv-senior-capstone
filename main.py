c922974069ddd7d93d32e7e846b5aca3a70613b1
linhvth <vuthihonglinh.work@gmail.com>
2024-03-23 09:07:24 +0700

    perform a test on a square function

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ import numpy as np
 import math
 from numpy.linalg import norm
 
-from sgd import *
+from SGD import *
 from data_generation import *
-from models import *
 
 
 ### LogReg Test => move to test
 
 
 ### Test 1: Simple convex functions
-f = lambda x: x**2
-df = lambda x: 2*x
-
+def square_function():
+  f = lambda x: (x-2)**2
+  df = lambda x: 2*(x-2)
+  return f, df
 
 ### Test 2: Non-convex functions
 def rosenbrock(X):
@@ -30,8 +30,18 @@ def rosenbrock(X):
   Rosenbrock function: 
     f(x) = sum((a_i - x[i-1])^2) for i = 1 to n-1, a = [1, 1.2, ..., n]
   """
-  a = np.array([1] + [1.2 for _ in range(len(X) - 1)])
-  return np.sum((a - X[:-1])**2) + 100 * np.sum((X[1:] - a**2)**2)
+  return
 
 
 ### Test 3: Perceptron
+
+### Perform tests
+if __name__ == '__main__':
+  f, df = square_function()
+  optimizer = SGD()
+  optimizer.optimize(f, df, n_dims=1, step_size=1e-2, max_iter=1000, 
+                     max_stream=100, epsilon=1e-5, tolerance=1e-2)
+  print(optimizer.get_last_points())
+  print(optimizer.get_solution())
+
+  """Comment: both epsilon and tolerance affect to the solution obtain by SGD"""
